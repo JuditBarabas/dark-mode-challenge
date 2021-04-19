9cc5457361098f4af1531e2e98c382f1291ef486
JuditBarabas <judit.barabas1@gmail.com>
2021-04-19 18:26:24 +0200

    shorten theme switcher button rendering

--- src/partials/ThemeSwitcher.js
+++ src/partials/ThemeSwitcher.js
@@ -7,14 +7,11 @@ export default class ThemeSwitcher extends React.Component{
     render() {
         return (
             <button className="app__dark-mode-btn icon level-right" onClick={this.props.changeMode}>
-                
-                { this.props.darkMode ? (
-                    <FontAwesomeIcon icon={faSun} color={'#FFA500'}/>
-                    )
-                    : (
-                    <FontAwesomeIcon icon={faMoon} color={'#4D5B6B'}/>
-                    )
-                }  
+
+                <FontAwesomeIcon 
+                    icon={this.props.darkMode ? faSun : faMoon} 
+                    color={this.props.darkMode ? '#FFA500' : '#4D5B6B'}
+                />
                 
             </button>
         );
